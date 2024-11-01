cb37142f60db5ff0a28b25310e41bf22110322d8
Андрей <dadykin016@gmail.com>
2024-11-01 11:48:18 +0300

    практическое задание

---
+++ main_1.py
@@ -0,0 +1,5 @@
+name_course = 'Курс:Python'
+number_of_tasks= 12
+number_of_hours_spent=1.5
+time_for_one_task =number_of_hours_spent/number_of_tasks
+print(name_course,',','всего задач:', number_of_tasks,',','затрачено часов:', number_of_hours_spent,',','среднее время выполнения', time_for_one_task,'часа.')
