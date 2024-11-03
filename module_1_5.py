8dd017dfb5cf3dfacbebf45b397a04a7883bbbb7
Андрей <dadykin016@gmail.com>
2024-11-03 15:26:19 +0300

    практическое задание

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+tuple_immutable_var =(1,2, True,"String")
+print('immutable var:',tuple_immutable_var)
+#tuple_immutable_var_[0] =200
+#print('immutable var:',tuple_immutable_var)
+#Кортеж не поддерживает обращение по элементам.
+mutable_list=[1,2,5,'a','b','c','Modified']
+print('mutable list:', mutable_list)
+mutable_list [4]= 33
+print('mutable list:', mutable_list)
