93819ba8dde3d8ad0a2a3c0f428ca101aafc5d45
Андрей <dadykin016@gmail.com>
2024-11-04 21:55:05 +0300

    практическое задание

--- module_1_7.py
+++ module_1_7.py
@@ -2,10 +2,10 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students=list(students)
 students = sorted(students)
-average_grades = {}
+average_scores = {}
 for i in range(len(students)):
-    student = list(students)[i]
-    grades_list = grades[i]
-    average_grade = sum(grades_list) / len(grades_list)
-    average_grades[student] = average_grade
-print(average_grades)
+ student = list(students)[i]
+ grades_list = grades[i]
+ average_score = sum(grades_list) / len(grades_list)
+ average_scores[student] = average_score
+print(average_scores)
