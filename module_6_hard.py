2093fc177456b15f42c81bca4519bec43e0e7c1c
Семья <dadykin016@gmail.com>
2024-12-25 22:02:36 +0300

    практическое задание

--- module_6_hard.py
+++ module_6_hard.py
@@ -81,7 +81,7 @@ class Cube(Figure):
 
     def get_volume(self):
         edge_length = self.get_sides()[0]
-        return edge_length ** 3  
+        return edge_length ** 3
 
 
 circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
