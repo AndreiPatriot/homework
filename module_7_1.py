10d25ed31298894c86f667f8291ca7291185b54a
Семья <som66@mail.ru>
2024-12-30 18:47:14 +0300

    практическое задание

--- module_7_1.py
+++ module_7_1.py
@@ -1,43 +1,43 @@
+from pprint import pprint
+
+
 class Product:
-    def __init__(self, name, weight, category):
-        self.name = str(name)
-        self.weight = float(weight)
-        self.category = str(category)
+    def __init__(self, name: str, weight: float, category: str):
+        self.name = name
+        self.weight = weight
+        self.category = category
 
     def __str__(self):
-        return f'{self.name}, {self.weight}, {self.category}'
+        return (f'{self.name}; {self.weight}; {self.category}')
 
 
 class Shop:
-    __file_name = 'products.txt'
-    file = open(__file_name, 'w')
-    file.close()
-
+    def __init__(self):
+        self.__file_name = 'products.txt'
 
     def get_products(self):
+        self.__file_name = 'products.txt'
         file = open(self.__file_name, 'r')
-        prod_str = file.read()
+        products = file.read()
         file.close()
-        return prod_str
+
+        return products
 
     def add(self, *products):
         for product in products:
-            _products = self.get_products()
-            if product.name in _products:
-                print(f'Продукт {product.name} уже есть в магазине')
+            if product.name not in self.get_products():
+                file = open(self.__file_name, 'a')
+                file.write(product.__str__() + '\n')
+
             else:
-                _file = open(self.__file_name, 'a')
-                _file.write(f'{product}\n')
-                _file.close()
+                print(f'Продукт {product.name} уже есть в магазине')
 
 
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
-print(p1) # __str__
-print(p2) # __str__
-print(p3) # __str__
-s1.add(p1, p2, p3)
 
+print(p2) # __str__
+s1.add(p1, p2, p3)
 print(s1.get_products())
